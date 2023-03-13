6955a7a1bf1e7b83de52ee4c7aa45c7db4962166
veekeey <meligavictoria@gmail.com>
2023-03-13 23:22:00 +0000

    checkkkk

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-if (process.argv.length > 3) {
-  console.log('Arguments found');
-} else if (process.argv.length === 3) {
+if (process.argv.length === 1) {
   console.log('Argument found');
+} else if (process.argv.length > 1) {
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
